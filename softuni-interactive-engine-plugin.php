e7e6ab47f1d99de58ffb01846dea7cfcd8960549
Svetlin Nakov <nakov@users.noreply.github.com>
2021-09-06 20:39:24 +0300

    Added short codes

--- softuni-interactive-engine-plugin.php
+++ softuni-interactive-engine-plugin.php
@@ -10,12 +10,15 @@
  * 
  * The shortcode [slack-url] returns the previously assigned Slack URL for the current course.
  * 
+ * The shortcode [hide-element-for-membership levels=3|5|7 id=admission-exam-banner] hides given HTML element (by ID),
+ * when the current user has any of the specified membership levels.
+ * 
  * Version: 1.0
  * Author: Svetlin Nakov
  * Author URI: https://nakov.com
  */
 
-function create_engine_link($attribs) {
+function create_engine_link_shortcode($attribs) {
     $courseId = $attribs['course'] ?? 0;
     $sectionId = $attribs['section'] ?? 0;
     global $current_user;
@@ -29,21 +32,43 @@ function create_engine_link($attribs) {
     return $engineUrl;
 }
 
-add_shortcode('engine', 'create_engine_link');
+add_shortcode('engine', 'create_engine_link_shortcode');
 
 
-function set_slack_url($attribs) {
+function set_slack_url_shortcode($attribs) {
     global $slackUrl;
     $slackUrl = $attribs['url'] ?? home_url();
 }
 
-add_shortcode('set-slack-url', 'set_slack_url');
+add_shortcode('set-slack-url', 'set_slack_url_shortcode');
 
 
-
-function get_slack_url($attribs) {
+function get_slack_url_shortcode($attribs) {
     global $slackUrl;
     return $slackUrl;
 }
 
-add_shortcode('slack-url', 'get_slack_url');
+add_shortcode('slack-url', 'get_slack_url_shortcode');
+
+
+function hide_element_for_membership_shortcode($attribs) {
+    if (is_admin()) {
+        // Do nothing when in 'edit page' mode
+        return "";
+    }
+
+    $levels = $attribs['levels'] ?? '';
+    $membershipLevels = explode('|', $levels);
+    $elementId = $attribs['id'] ?? 'missing-element-id';
+    if (pmpro_hasMembershipLevel($membershipLevels)) {
+        // Hide the specified element with CSS display:none
+        return 
+            "<style>
+                #{$elementId} {
+                    display: none;                
+                }
+            </style>";
+    }
+}
+
+add_shortcode('hide-element-for-membership', 'hide_element_for_membership_shortcode');
